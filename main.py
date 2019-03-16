4895a289aa9d3b64195e5d5366215858b3297498
Simon Pinfold <simon@uint8.me>
2019-03-16 14:08:07 +1300

    Add Zappa deployment via CI

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import logging
 from datetime import datetime
 from flask import Flask, render_template, url_for
 from flask_bootstrap import Bootstrap
-from typing import List, Optional
+from typing import Optional
 
-from overtrack.apex.collect.apex_game import ApexGame
-from overtrack.apex.data import Champion
+from models.apex_game_summary import ApexGameSummary
 from overtrack.util import s2ts
+from overtrack.util.logging_config import config_logger
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 logger = logging.getLogger(__name__)
+
+config_logger('apextrack-web', logging.INFO, False)
 
 
 def to_ordinal(n: int) -> str:
@@ -21,8 +23,8 @@ def to_ordinal(n: int) -> str:
     return f'{n}{suffixes.get(i, "th")}'
 
 
-def image_url(champ: Optional[Champion]) -> str:
-    return url_for('static', filename=f'images/{champ.name.lower()}.png' if champ else 'images/unknown.png')
+def image_url(champ: Optional[str]) -> str:
+    return url_for('static', filename=f'images/{champ}.png' if champ else 'images/unknown.png')
 
 
 def strftime(t: float):
@@ -39,7 +41,7 @@ def duration(t: float):
 @app.route("/games")
 def games_list():
     context = {
-        'games': games,
+        'games': ApexGameSummary.user_id_time_index.query(-1),
         'to_ordinal': to_ordinal,
         's2ts': duration,
         'strftime': strftime,
@@ -47,31 +49,3 @@ def games_list():
     }
     return render_template('games.html', **context)
 
-
-def _load_sample_games() -> List[ApexGame]:
-    from overtrack.apex.collect.apex_game_extractor import ApexGameExtractor
-    from overtrack.source.video import VideoFrameExtractor
-    from overtrack.util import referenced_typedload
-    import json
-    from overtrack.frame import Frame
-
-    logger.info('Loading frames')
-    with open("C:/Users/simon/workspace/overtrack_2/games/apex_eeveea_apex_19-03-13.json") as f:
-        frames = referenced_typedload.load(
-            json.load(f),
-            List[Frame],
-            source_type=VideoFrameExtractor.VideoFrameMetadata,
-            support_overwatch=False
-        )
-    logger.info(f'Loaded {len(frames)} frames')
-
-    ex = ApexGameExtractor(keep_games=True)
-
-    for f in frames:
-        ex.on_frame(f)
-    ex.finish()
-
-    return ex.games
-
-
-games = _load_sample_games()
