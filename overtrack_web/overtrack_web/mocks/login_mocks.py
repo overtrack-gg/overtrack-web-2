694dd315fa7420cacddeedd55bb646ed191d541f
Jessica Mortimer <git@jess.pink>
2020-02-27 17:49:54 +1300

    Fix local Flask app

--- overtrack_web/overtrack_web/mocks/login_mocks.py
+++ overtrack_web/overtrack_web/mocks/login_mocks.py
@@ -14,9 +14,11 @@ class MockUser(NamedTuple):
     apex_last_season: int = apex_data.current_season.index
     apex_last_game_ranked: bool = True
     apex_seasons: List[int] = list(apex_data.seasons.keys())
+    apex_games: int = 1
 
     overwatch_last_season: int = overwatch_data.current_season.index
     overwatch_seasons: List[int] = list(overwatch_data.seasons.keys())[-5:]
+    overwatch_games: int = 1
 
     subscription_active: bool = bool(random.randint(0, 1))
     superuser: bool = True
