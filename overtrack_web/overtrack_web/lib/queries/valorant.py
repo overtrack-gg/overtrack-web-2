7bce37548f29c80809e3f8a7ec34e7993dcd55f6
Simon Pinfold <simon@uint8.me>
2020-06-07 15:01:23 +1200

    Valorant: Use a new db session for each query

--- overtrack_web/overtrack_web/lib/queries/valorant.py
+++ overtrack_web/overtrack_web/lib/queries/valorant.py
@@ -1,3 +1,6 @@
+from contextlib import contextmanager
+from typing import ContextManager
+
 import logging
 
 import os
@@ -7,7 +10,8 @@ try:
     from overtrack.valorant.relational import queries
 
     from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.orm import sessionmaker, Session
+
     engine = create_engine((
         f'postgresql://'
         f'overtrack'
@@ -22,15 +26,24 @@ try:
         echo=True,
         executemany_mode='batch',
     )
-    db_session = sessionmaker(bind=engine)()
+
+    session_maker = sessionmaker(bind=engine)
+
+    @contextmanager
+    def db_session() -> ContextManager[Session]:
+        sess = session_maker()
+        yield sess
+        sess.close()
 except:
     logging.exception('Failed to connect to database - no db session created')
     db_session = None
 
 
 def get_winrates(user_id: int) -> MapAgentWinrates:
-    return queries.agent_map_winrates(db_session, user_id, game_version_atleast='01.00.0')
+    with db_session() as sess:
+        return queries.agent_map_winrates(sess, user_id, game_version_atleast='01.00.0')
 
 
 def get_average_winrates() -> MapAgentWinrates:
-    return queries.agent_map_winrates(db_session, None, game_version_atleast='01.00.0')
+    with db_session() as sess:
+        return queries.agent_map_winrates(sess, None, game_version_atleast='01.00.0')
