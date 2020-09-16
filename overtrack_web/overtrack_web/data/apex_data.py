4e0bcd39eb3a74bf0c4857b6be85ef2b1b0e3c8b
Simon Pinfold <simon@uint8.me>
2020-09-16 16:39:41 +1200

    Add season 6

--- overtrack_web/overtrack_web/data/apex_data.py
+++ overtrack_web/overtrack_web/data/apex_data.py
@@ -55,6 +55,7 @@ seasons: Dict[int, ApexSeason] = {
     3: ApexSeason(index=3, start=1569956446.0, end=1580839200.0),
     4: ApexSeason(index=4, start=1580839200.0, end=1589302800.0),
     5: ApexSeason(index=5, start=1589302800.0, end=1597766400.0),
+    6: ApexSeason(index=6, start=1597770000.0, end=1700000000.0),
 
     1005: ApexSeason(1005, start=1589302800.0, end=1597766400.0, season_name='Season 5 Duos', has_ranked=False),
 }
