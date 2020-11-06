ccbb4298c331d415acc284cd92293538e53d8686
SDCore <sdcored@gmail.com>
2020-11-06 00:56:21 -0600

    Fixed final S6 trios/duos date and added S7 trios/duos date

--- overtrack_web/overtrack_web/data/apex_data.py
+++ overtrack_web/overtrack_web/data/apex_data.py
@@ -55,10 +55,12 @@ seasons: Dict[int, ApexSeason] = {
     3: ApexSeason(index=3, start=1569956446.0, end=1580839200.0),
     4: ApexSeason(index=4, start=1580839200.0, end=1589302800.0),
     5: ApexSeason(index=5, start=1589302800.0, end=1597726800.0),
-    6: ApexSeason(index=6, start=1597726800.0, end=1604984400.0),
+    6: ApexSeason(index=6, start=1597726800.0, end=1604548800.0),
+    7: ApexSeason(index=7, start=1604548800.0, end=1612324800.0),
 
     1005: ApexSeason(1005, start=1589302800.0, end=159772680008, season_name='Season 5 Duos', has_ranked=False),
-    1006: ApexSeason(1006, start=1597726800.0, end=1604984400.0, season_name='Season 6 Duos', has_ranked=False),
+    1006: ApexSeason(1006, start=1597726800.0, end=1604548800.0, season_name='Season 6 Duos', has_ranked=False),
+    1007: ApexSeason(1007, start=1604548800.0, end=1612324800.0, season_name='Season 7 Duos', has_ranked=False),
 }
 current_season = seasons[sorted(seasons.keys())[-1]]
 try:
